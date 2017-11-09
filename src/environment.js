b4594f2fef63a07b1880559de99c81d89582a8e0
Jan Michael Auer <jan.auer@sentry.io>
2017-11-09 11:02:39 -0800

    fix: Log environments better

--- src/environment.js
+++ src/environment.js
@@ -2,13 +2,14 @@ const requireDir = require('require-dir');
 const logger = require('./logger');
 
 function getEnvironment() {
+  logger.debug('Initializing CI environment');
+
   const hookBase = process.env.HOOK_BASE;
   if (!hookBase) {
     logger.error('Missing ZEUS_HOOK_BASE environment variable');
     process.exit(1);
   }
 
-  logger.debug('Initializing CI environment');
   const environments = requireDir('environments');
   logger.debug(`Found environments: ${Object.keys(environments)}`);
 
