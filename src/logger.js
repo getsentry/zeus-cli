52d0c74d4f5c011c4d200444f0b308dbd28b1c50
Jan Michael Auer <jan.auer@sentry.io>
2017-11-09 11:02:27 -0800

    fix: Add a console transport to the logger

--- src/logger.js
+++ src/logger.js
@@ -1,7 +1,8 @@
 const winston = require('winston');
 
 const logger = new winston.Logger({
-  level: process.env.LOG_LEVEL || 'warning'
+  level: process.env.LOG_LEVEL || 'warning',
+  transports: [new winston.transports.Console()]
 });
 
 logger.cli();
