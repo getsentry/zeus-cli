e82e3aaa098763b05cbe825c4fc86496956307ff
Jan Michael Auer <jan.auer@sentry.io>
2017-11-25 17:12:34 +0100

    test: Add a manual mock for the form-data package

---
+++ src/__mocks__/form-data.js
@@ -0,0 +1,17 @@
+/* eslint-env jest */
+/* eslint-disable class-methods-use-this */
+
+module.exports = class FormData {
+  constructor(options) {
+    this.options = options;
+    this.data = {};
+  }
+
+  append(field, value) {
+    this.data[field] = value;
+  }
+
+  getHeaders() {
+    return { 'content-type': 'multipart/form-data; boundary=---feedface' };
+  }
+};
