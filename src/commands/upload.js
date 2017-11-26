073f8ed7f1b123b6c23d054038a0c514a74cb7c9
Jan Michael Auer <jan.auer@sentry.io>
2017-11-26 21:47:35 +0100

    fix: Node 4 compatibility for the upload command

--- src/commands/upload.js
+++ src/commands/upload.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { createReadStream, existsSync } = require('fs');
+const fs = require('fs');
 const env = require('../environment');
 const logger = require('../logger');
 const Zeus = require('../sdk');
@@ -9,7 +9,7 @@ module.exports = {
   command: ['upload <file>', 'u'],
   description: 'Upload a build artifact',
 
-  builder: yargs =>
+  builder: /* istanbul ignore next */ yargs =>
     yargs
       .positional('file', {
         description: 'Path to the artifact',
@@ -31,25 +31,25 @@ module.exports = {
         alias: 'build',
       }),
 
-  handler: async argv => {
-    try {
-      if (!existsSync(argv.file)) {
-        throw new Error(`File does not exist: ${argv.file}`);
-      }
+  handler: argv => {
+    const zeus = new Zeus({ url: argv.url, token: argv.token });
+    const promise = !fs.existsSync(argv.file)
+      ? Promise.reject(new Error(`File does not exist: ${argv.file}`))
+      : zeus.uploadArtifact({
+          build: argv.build || env.buildId,
+          job: argv.job || env.jobId,
+          file: fs.createReadStream(argv.file),
+          type: argv.type,
+        });
 
-      const zeus = new Zeus({ url: argv.url, token: argv.token });
-      const result = await zeus.uploadArtifact({
-        build: argv.build || env.buildId,
-        job: argv.job || env.jobId,
-        file: createReadStream(argv.file),
-        type: argv.type,
+    return promise
+      .then(result => {
+        logger.info('Artifact upload completed');
+        logger.info(`URL: ${result.download_url}`);
+      })
+      .catch(e => {
+        logger.error(`Artifact upload failed: ${e.message}`);
+        process.exitCode = 1;
       });
-
-      logger.info('Artifact upload completed');
-      logger.info(`URL: ${result.download_url}`);
-    } catch (e) {
-      logger.error(`Artifact upload failed: ${e.message}`);
-      process.exitCode = 1;
-    }
   },
 };
