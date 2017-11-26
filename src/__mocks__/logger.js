89deb07e399023caad8c0fa2855be8768345e08d
Jan Michael Auer <jan.auer@sentry.io>
2017-11-26 17:15:21 +0100

    test: Add mocks for environment and logger

---
+++ src/__mocks__/logger.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* eslint-env jest */
+const logger = jest.fn();
+logger.error = logger;
+logger.warn = logger;
+logger.info = logger;
+logger.debug = logger;
+module.exports = logger;
