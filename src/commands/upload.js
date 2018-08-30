796df7de96bf1ad488a37a79f4683fdfce0a90a8
Anton Ovchinnikov <anton@tonyo.info>
2018-08-30 16:37:30 +0200

    feat: Retry artifact uploads by default

--- src/commands/upload.js
+++ src/commands/upload.js
@@ -1,9 +1,62 @@
 'use strict';
 
 const fs = require('fs');
+
+const promiseRetry = require('promise-retry');
+const zeus = require('@zeus-ci/sdk');
+
 const getEnv = require('../environment').getEnvironment;
 const logger = require('../logger');
-const zeus = require('@zeus-ci/sdk');
+const constants = require('../constants');
+
+/**
+ * Attempts to upload the provided artifacts to Zeus
+ *
+ * In case of certain failures, the upload will be attempted again after a delay.
+ * We pass the factory function instead of an Artifact object, because artifact
+ * objects have internal references to read streams, that might be depleted.
+ *
+ * @param {zeus.Client} client - Zeus client
+ * @param {} artifactFactory - The factory that produces Artifact objects
+ * @param {number} attempts - The total number of attempts to make in case of failures
+ * @returns A promise that resolves when uploading is done
+ */
+function attemptUploadArtifact(client, artifactFactory, _attempts) {
+  // Node 4 compatibility
+  const attempts =
+    _attempts === undefined ? constants.UPLOAD_RETRY_ATTEMPTS : _attempts;
+  return promiseRetry(
+    (retry, number) => {
+      const artifact = artifactFactory();
+      return client.uploadArtifact(artifact).catch(e => {
+        if (constants.UPLOAD_RETRY_RESPONSES.indexOf(e.message) > -1) {
+          // Output the messages only if it's not the last attempt
+          if (number < attempts) {
+            logger.error(
+              `Upload of artifact "${
+                artifact.file.path
+              }" failed with the following error: "${e.message}"`
+            );
+            logger.warn(
+              `Retries left: ${attempts -
+                number}. Retrying in about ${Math.floor(
+                constants.UPLOAD_RETRY_TIMEOUT / 1000.0
+              )} seconds...\n`
+            );
+          }
+          retry(e);
+        } else {
+          throw e;
+        }
+      });
+    },
+    {
+      retries: attempts - 1,
+      minTimeout: constants.UPLOAD_RETRY_TIMEOUT,
+      maxTimeout: constants.UPLOAD_RETRY_TIMEOUT * 1.1,
+    }
+  );
+}
 
 module.exports = {
   command: ['upload <file...>', 'u'],
@@ -34,9 +87,15 @@ module.exports = {
         description: 'Unique id of the build in CI',
         type: 'string',
         alias: 'build',
+      })
+      .option('R', {
+        description: 'Do not retry failed uploads',
+        type: 'boolean',
+        alias: 'no-retry',
       }),
 
   handler: argv => {
+    logger.debug('Argv:', argv);
     const client = new zeus.Client({
       url: argv.url,
       token: argv.token,
@@ -46,13 +105,17 @@ module.exports = {
     const uploads = argv.file.map(file => {
       const promise = !fs.existsSync(file)
         ? Promise.reject(new Error(`File does not exist: ${file}`))
-        : client.uploadArtifact({
-            build: argv.build || env.buildId,
-            job: argv.job || env.jobId,
-            file: fs.createReadStream(file),
-            name: argv.name,
-            type: argv.type,
-          });
+        : attemptUploadArtifact(
+            client,
+            () => ({
+              build: argv.build || env.buildId,
+              job: argv.job || env.jobId,
+              file: fs.createReadStream(file),
+              name: argv.name,
+              type: argv.type,
+            }),
+            argv.noRetry ? 1 : constants.UPLOAD_RETRY_ATTEMPTS
+          );
 
       return promise
         .then(result => {
